8c48cff0ba5b51c3e812dfd671f56e5dc6230321
Christopher Jenkin <christopherjenkin64@gmail.com>
2024-11-15 17:34:55 -0500

    chore: test

--- Scripts/CharacterScripts/PlayerScripts/Player.gd
+++ Scripts/CharacterScripts/PlayerScripts/Player.gd
@@ -1,38 +1,43 @@
 extends CharacterBody2D
 
-@export var movementSpeed : float = 100
+@export var movement_speed: float = 100
 
-@onready var _animated_sprite = $AnimatedSprite2D
-var attackAnimationFinished = false
-var rightRelease = false
-func _physics_process(_delta):
+@onready var animated_sprite = $AnimatedSprite2D
+var attack_animation_finished = true
+var is_facing_right = true
+
+func _physics_process(delta):
 	var input_direction = Vector2(
-		Input.get_action_strength("right") - Input.get_action_strength("left"), 
-		Input.get_action_strength("down") - Input.get_action_strength("up"),
-	);
-	if (Input.is_action_pressed("attack") && rightRelease) && !attackAnimationFinished:
-		attackAnimationFinished=false
-		_animated_sprite.play("attack_right")
-	elif (Input.is_action_pressed("attack") && !rightRelease) && !attackAnimationFinished:
-		attackAnimationFinished=false
-		_animated_sprite.play("attack_left")
-	elif Input.is_action_pressed("right"):
-		attackAnimationFinished=false
-		_animated_sprite.play("right_run")
-		rightRelease=true
+		Input.get_action_strength("right") - Input.get_action_strength("left"),
+		Input.get_action_strength("down") - Input.get_action_strength("up")
+	)
+	velocity = input_direction * movement_speed
+	
+	# Update facing direction based on input
+	if Input.is_action_pressed("right"):
+		is_facing_right = true
+		if attack_animation_finished:
+			animated_sprite.play("right_run")
 	elif Input.is_action_pressed("left"):
-		attackAnimationFinished=false
-		_animated_sprite.play("left_run")
-		rightRelease=false
-	elif (Input.is_action_just_released("right") || rightRelease):
-		_animated_sprite.play("idle_right")
-	else:
-		_animated_sprite.play("idle_left")
-		rightRelease=false
-	velocity = input_direction*movementSpeed;
+		is_facing_right = false
+		if attack_animation_finished:
+			animated_sprite.play("left_run")
+	# Handle attack input
+	elif Input.is_action_just_pressed("attack") and attack_animation_finished:
+		attack_animation_finished = false
+		if is_facing_right:
+			animated_sprite.play("attack_right")
+		else:
+			animated_sprite.play("attack_left")
+	# Idle animation when not moving
+	elif attack_animation_finished:
+		if is_facing_right:
+			animated_sprite.play("idle_right")
+		else:
+			animated_sprite.play("idle_left")
 
-	move_and_slide();
-
+	# Move the character
+	move_and_slide()
 
 func _on_animated_sprite_2d_animation_finished():
-	attackAnimationFinished=true
+	attack_animation_finished = true
